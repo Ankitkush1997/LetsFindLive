2eaf5640de4b3b02c81ba79fb9000706a270413c
satishr-rajnale <satishraj.rajnale@gmail.com>
2022-06-17 11:41:25 +0530

    changes for url api

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,76 +1,135 @@
-import React, { useState } from 'react'
-import { Accordion, Card, InputGroup, Button, Modal, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Header from './Header';
+import React, { useState, useEffect } from "react";
+import {
+  Accordion,
+  Card,
+  InputGroup,
+  Button,
+  Modal,
+  Form,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import axios from "axios";
+
 const Home = () => {
-  
+  const [modalShow, setModalShow] = useState(false);
+  const [listItem, setListItem] = useState([]);
+  const [itemValues, setItemValues] = useState(" ");
 
-    const [modalShow, setModalShow] = useState(false);
-    const [listItem, setListItem] = useState([])
-    const [ itemValues, setItemValues] = useState(" ")
+  const handleSubmit = () => {
+    addUrl();
+    setModalShow(false);
+    // setListItem([...listItem, itemValues]);
+    console.log(listItem);
+  };
 
-    const handleSubmit = () => {
-      setModalShow(false)
-         setListItem([...listItem,itemValues])
-         console.log(listItem)
-    }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/api/google/search/allURL`)
+      .then((response) => {
+        // setFilteredDataApi(response?.data?.data);
+        setListItem(response?.data?.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
 
-    const filterdData = () =>{
-      
-        
-    }
+  console.log("listItem", listItem);
 
+  const addUrl = () => {
+    axios
+      .get(`http://localhost:5005/api/google/search/add/url/${itemValues}`)
+      .then((response) => {
+        // setFilteredDataApi(response?.data?.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+  const deleteUrl = (e) => {
+    console.log("val", e);
+    axios
+      .get(`http://localhost:5005/api/google/search/delete/${e}`)
+      .then((response) => {
+        // setFilteredDataApi(response?.data?.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   return (
     <>
-    <Header/>
-    <Accordion style={{display:"flex",justifyContent:"center"}} defaultActiveKey="0">
-      <Card style={{width:"50%"}}>
-        <Card.Header style={{display:"flex", justifyContent:"space-between"}}>
+      <Header />
+      <Accordion
+        style={{ display: "flex", justifyContent: "center" }}
+        defaultActiveKey="0"
+      >
+        <Card style={{ width: "50%" }}>
+          <Card.Header
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
             <div> Page/URL</div>
-            <Button className='btn-success' style={{ display: "grid",paddingBottom: "8px",placeItems: "center"}} variant="primary" onClick={() => setModalShow(true)}>
-        ADD
-      </Button>
-        </Card.Header>
-        
-        {listItem.map((e) => (
-        <Accordion.Collapse key={e.slice(2,)} eventKey="0">
-          <Card.Body style={{display:"flex",justifyContent:"space-between"}}>
-          <Link target="_blank" to={`/find/${e}`}>{e}</Link>
-          <Button onClick={(e) =>filterdData()} className='btn-danger'> Delete </Button>
-          </Card.Body>
-        </Accordion.Collapse>))}
-      </Card>
-      
-      <Modal
-      show={modalShow}
-      onHide={() => setModalShow(false)}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
+            <Button
+              className="btn-success"
+              style={{
+                display: "grid",
+                paddingBottom: "8px",
+                placeItems: "center",
+              }}
+              variant="primary"
+              onClick={() => setModalShow(true)}
+            >
+              ADD
+            </Button>
+          </Card.Header>
 
-      <Modal.Header closeButton>
-      </Modal.Header>
-      <Modal.Body>
-    
-      <InputGroup className="mb-2">
-        <Form.Control
-          onChange={(e) => setItemValues(e.target.value)}
-          placeholder="Enter URL to add..."
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={handleSubmit}>ADD</Button>
-      </Modal.Footer>
-    </Modal>
-    </Accordion>
+          {listItem.map((element) => (
+            <Accordion.Collapse key={element.slice(2)} eventKey="0">
+              <Card.Body
+                style={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <Link target="_blank" to={`/find/${element}`}>
+                  {element}
+                </Link>
+                <Button
+                  onClick={() => deleteUrl(element)}
+                  className="btn-danger"
+                >
+                  {" "}
+                  Delete{" "}
+                </Button>
+              </Card.Body>
+            </Accordion.Collapse>
+          ))}
+        </Card>
+
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton></Modal.Header>
+          <Modal.Body>
+            <InputGroup className="mb-2">
+              <Form.Control
+                onChange={(e) => setItemValues(e.target.value)}
+                placeholder="Enter URL to add..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleSubmit}>ADD</Button>
+          </Modal.Footer>
+        </Modal>
+      </Accordion>
     </>
-
-  )
-}
+  );
+};
 
 export default Home;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const googleRoutes = require('./routes/googleRoutes');
+const googleRoutes = require("./routes/googleRoutes");
 
 const app = express();
 
@@ -8,22 +8,22 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
 //-------------MIDDLEWARE----------
 
-app.use('/api/google', googleRoutes);
+app.use("/api/google", googleRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, console.log('Server running on port 5000'));
+app.listen(PORT, console.log("Server running on port 5005"));
